62b5e589e8bbef689fc4c3b681ce2de75433a4ea
ChrisGit1939 <104327958+ChrisGit1939@users.noreply.github.com>
2025-04-12 13:21:59 +0200

    Delete C/power bank, mini power storage/software directory

--- C/power bank, mini power storage/software/Interrupt_functions.c
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void timer1_setup(void)       // timer1 16-bit
-{
-	cli();
-	TCCR1B |= (1 << CS11);   // prescaler /8
-	TIMSK1 |= (1 << TOIE1);  // interrupt for overflow
-	sei();
-}
